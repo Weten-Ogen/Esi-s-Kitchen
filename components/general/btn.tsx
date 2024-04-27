3bbf190d7372abc049fa253de257b593abb9a6b2
Weten-Ogen <alphaoware@gmail.com>
2024-04-27 02:39:29 +0000

    first commit

--- components/general/btn.tsx
+++ components/general/btn.tsx
@@ -5,7 +5,7 @@ import { btn } from '@/next'
 import { cn } from '@/lib/utils'
 
 export default function Btn({name,children,className,vatype}:btn) {
-  if(!children) return <Button variant={vatype} className={cn('',className)}>{name}</Button>
+  if(!children) return <Button variant={vatype ? vatype : "default"} className={cn('',className)}>{name}</Button>
   return <Button>
     <>
     {children}

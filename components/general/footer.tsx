688934fd35248e64eacba76058b750ec4c58af6d
Weten-Ogen <alphaoware@gmail.com>
2024-05-02 12:20:34 +0000

    first commit

--- components/general/footer.tsx
+++ components/general/footer.tsx
@@ -7,16 +7,16 @@ export default function Footer() {
         <div className="text-center">
             <p className='capitalize   text-2xl tracking-wider font-medium'>get in touch</p>
             <div className='my-3'>
-                <h4 className='font-medium text-lg tracking-wider'>mailing address</h4>
+                <h4 className='font-medium text-lg  text-orange-500 tracking-wider'>mailing address</h4>
                 <pre className='text-lg' > Gbawe c.p opp Immanuel Methods</pre>
                 <pre className='text-lg'>Church & Tema Comm 11 station</pre>
             </div>
             <div className='mb-3'>
-                <h4  className="text-lg tracking-wide">email address</h4>
+                <h4  className="text-lg text-orange-500 tracking-wide">email address</h4>
                 <pre className="text-lg">esiskitchen@gmail.com</pre>
             </div>
             <div className="text-lg">
-                <h4>phone number</h4>
+                <h4 className='text-lg tracking-wide text-orange-500'>phone number</h4>
                 <pre className="text-lg">
                     +233-23-3940-156
                 </pre>

ecd0b7c79dd4a9e54bd29f5ea2e0f312d4a84ade
Weten-Ogen <alphaoware@gmail.com>
2024-05-14 10:42:16 +0000

    first commit

--- app/actions/actions.ts
+++ app/actions/actions.ts
@@ -5,12 +5,12 @@ import { signIn, signOut } from "@/auth"
 
 export async function googleSignIn() {
      await signIn("google",{redirectTo: "/bookings"});
-     return 0
+     
 }
 
 
 export async function googleSignOut() {
     await signOut();
-    return 1
+    
 }
 

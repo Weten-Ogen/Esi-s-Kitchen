482fdbe298532f39a0b8275e0ca2b2e9200d4bc0
Weten-Ogen <alphaoware@gmail.com>
2024-05-14 01:03:44 +0000

    first commit

--- lib/prisma.ts
+++ lib/prisma.ts
@@ -4,9 +4,7 @@ import {PrismaNeon } from '@prisma/adapter-neon';
 
 const prismaClientSingleton = () => {
 
-  const neon = new Pool({connectionString:process.env.POSTGRES_PRISMA_URL!});
-  const adapter = new PrismaNeon(neon);
-  return  new PrismaClient({adapter});
+  return  new PrismaClient();
 }
 
 declare global{

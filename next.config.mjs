a9b1555290d1ae5f4b512dcfd2ff889b573f991a
Weten-Ogen <alphaoware@gmail.com>
2024-05-14 10:04:27 +0000

    first commit

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,16 @@
 import { withNextVideo } from "next-video/process";
+import { hostname } from "os";
 /** @type {import('next').NextConfig} */
 
-const nextConfig = {};
+const nextConfig = {
+    images: {
+        remotePatterns: {
+            protocol: "https",
+            hostname: "lh3.googleusercontent.com",
+            port: "",
+            pathname:"/a/**",
+        }
+    }
+};
 
 export default withNextVideo(nextConfig);

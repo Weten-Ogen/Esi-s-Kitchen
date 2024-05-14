9b8795769f9ba854795c2f6878b7a3f7bdcb923e
Weten-Ogen <alphaoware@gmail.com>
2024-05-14 00:49:57 +0000

    first commit

--- lib/prisma.ts
+++ lib/prisma.ts
@@ -3,9 +3,10 @@ import {Pool } from '@neondatabase/serverless';
 import {PrismaNeon } from '@prisma/adapter-neon';
 
 const prismaClientSingleton = () => {
-  const neon = new Pool({connectionString:process.env.POSTGRES_PRISMA_URL});
+
+  const neon = new Pool({connectionString:process.env.POSTGRES_PRISMA_URL!});
   const adapter = new PrismaNeon(neon);
-  return new PrismaClient({adapter});
+  return  new PrismaClient({});
 }
 
 declare global{
@@ -13,7 +14,7 @@ declare global{
 } 
 
 const prisma = globalThis.prismaGlobal ?? prismaClientSingleton()
-
+ 
 export default prisma
 
 if (process.env.NODE_ENV !== 'production') globalThis.prismaGlobal = prisma

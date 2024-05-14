10c33c2a4722339ad7a8d3b4e2e44e5f670645df
Weten-Ogen <alphaoware@gmail.com>
2024-05-14 02:30:36 +0000

    first commit

--- components/custom/signout.tsx
+++ components/custom/signout.tsx
@@ -1,14 +1,15 @@
+"use client"
 import { googleSignOut } from '@/app/actions'
 import React from 'react'
 import { Button } from '../ui/button'
+import { signOut } from 'next-auth/react'
 
 export default function SignOutBtn() {
   return (
-    <form action={googleSignOut}>
         <Button
+        onClick={async() => signOut()}
         className='tracking-wide text-lg cap' 
         variant={"outline"} 
         type="submit">log out</Button>
-    </form>
   )
 }

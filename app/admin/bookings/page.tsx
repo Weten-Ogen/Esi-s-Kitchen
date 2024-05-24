63003d009b3e21b65c430fa27a2b4488b71b0062
Weten-Ogen <alphaoware@gmail.com>
2024-05-24 05:43:06 +0000

    first commit

--- app/admin/bookings/page.tsx
+++ app/admin/bookings/page.tsx
@@ -3,10 +3,10 @@ import React from 'react'
 
 export default async function AdminBookings(){
    const bookings = await  prisma.bookings.findMany({})
-   console.log(bookings)
+   
   return (
     <section>
-
+      <div>{JSON.stringify(bookings)}</div>
     </section>
   )
 }

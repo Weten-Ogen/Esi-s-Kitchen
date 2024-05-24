fa56c3246b42c82ea8a3ee245d6ab8896f2c5cf0
Weten-Ogen <alphaoware@gmail.com>
2024-05-24 11:39:12 +0000

    first commit

--- app/actions/actions.ts
+++ app/actions/actions.ts
@@ -4,7 +4,7 @@ import prisma from "@/lib/prisma";
 import { revalidatePath } from "next/cache";
 import { redirect } from "next/navigation";
 
-export async function appointment (input:any){
+export async function appointment(input:any){
     await prisma.booking.create(
         {
             data:input,

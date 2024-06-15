5b9cfd1ef8827f6fe7acece2222d30d7d49bc834
Weten-Ogen <alphaoware@gmail.com>
2024-06-15 03:43:22 +0000

    almost done

--- components/custom/signout.tsx
+++ components/custom/signout.tsx
@@ -4,14 +4,16 @@ import { Button } from '../ui/button'
 import { googleSignOut } from '@/app/actions/actions'
 import { useFormStatus } from 'react-dom'
 import { Loader } from 'lucide-react'
-import {usePathname} from 'next/navigation'
+import {usePathname, useRouter} from 'next/navigation'
 import { toast } from 'sonner'
 
 export default function SignOutBtn() {
   const pathname = usePathname()
+  const router = useRouter()
   const {pending} = useFormStatus();
   const handlesignout = async() => {
     await googleSignOut(pathname)
+    router.refresh()
     toast.success('logged out successfully.')
   }
 

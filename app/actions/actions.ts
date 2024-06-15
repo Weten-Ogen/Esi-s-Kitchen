8a79742bf8a9cf8fa7d3bb683d06638e837647f9
Weten-Ogen <alphaoware@gmail.com>
2024-06-15 04:47:44 +0000

    almost done

--- app/actions/actions.ts
+++ app/actions/actions.ts
@@ -59,7 +59,7 @@ export async function googleSignIn(formData:FormData) {
 
 export async function googleSignOut(stand:string) {
     await signOut({redirectTo:"/"})
-    revalidatePath(`/${stand}`)
+    revalidatePath("/")
     
 }
 interface propss{
